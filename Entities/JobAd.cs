01186db2f8efb45bfccee6b5fa16f9fdece34567
Christos Arampatzis <xr.aram@yahoo.com>
2022-07-11 21:40:34 +0300

    Add project files.

---
+++ Entities/JobAd.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class JobAd
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DatePosted { get; set; }
+        public int? Salary { get; set; }
+        public JobType Type { get; set; }
+        public int MyProperty { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
